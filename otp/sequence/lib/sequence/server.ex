abb675a188c06d9b8ae60cebbe3443f7fb67d7b9
hackrole <daipeng123456@gmail.com>
2017-03-15 22:37:42 +0800

    add GenServer sequence server. not add client code now

---
+++ otp/sequence/lib/sequence/server.ex
@@ -0,0 +1,19 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number+1}
+  end
+
+  def handle_call({:set_number, new_number}, _from, _current_number) do
+    {:reply, new_number, new_number}
+  end
+
+  def handle_call({:factors, number}, _form, _current_numbner) do
+    {:reply, {:factors_of, number, number}, []}
+  end
+
+  def handle_cast({:increment_number, delta}, current_number) do
+    {:noreply, current_number + delta}
+  end
+end
