74e74d652b0f8c68b83230a4e08951ef0dcf98fe
hackrole <daipeng123456@gmail.com>
2017-03-16 22:50:52 +0800

    finish macros

---
+++ macros/my.exs
@@ -0,0 +1,15 @@
+defmodule My do
+  def myif(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+
+    case condition do
+      val when val in [false, nil]
+        -> else_clause
+      _otherwise
+        -> do_clause
+    end
+  end
+end
+
+My.myif 1==2, do: (IO.puts "1==2", else: (IO.puts "1 != 2"))
